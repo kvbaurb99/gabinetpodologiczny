70d017a323c2c782aeac2953149e7b0986f92bc0
kvbaurb99 <kvbaurbanski2@gmail.com>
2025-04-11 17:49:13 +0200

    updates

--- src/components/header/styles/slide.ts
+++ src/components/header/styles/slide.ts
@@ -241,6 +241,8 @@ export const PaginationContainer = styled.div`
 `;
 
 export const PaginationInner = styled.div`
+position: relative;
+bottom: 0.375rem;
   width: 90%; /* Mobile-first: 90% width */
   margin: 0 auto;
   padding: 0 1rem;
