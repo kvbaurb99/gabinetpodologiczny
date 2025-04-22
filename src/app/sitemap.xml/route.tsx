997adfb87f69b07a81d321c30db2173d69d48e78
kvbaurb99 <kvbaurbanski2@gmail.com>
2025-04-22 20:22:21 +0200

    update sitemap

--- src/app/sitemap.xml/route.tsx
+++ src/app/sitemap.xml/route.tsx
@@ -2,13 +2,12 @@ import { getArticles } from "@/server/getArticles";
 
 export async function GET() {
   const articles = await getArticles();
+  const date = new Date();
+  const offset = date.getTimezoneOffset() * 60000; // offest in ms
+  const localISOTime = new Date(date.getTime() - offset).toISOString();
 
   const sitemapIndex = articles
     .map((article) => {
-      const date = new Date();
-      const offset = date.getTimezoneOffset() * 60000; // offest in ms
-      const localISOTime = new Date(date.getTime() - offset).toISOString();
-
       return `
     <url>
       <loc>https://podologjaworze.pl/blog/${article.slug}</loc>
@@ -22,6 +21,18 @@ export async function GET() {
 
   let xml = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+      <loc>https://podologjaworze.pl</loc>
+      <lastmod>${localISOTime}</lastmod>
+      <changefreq>daily</changefreq>
+      <priority>0.8</priority>
+    </url>
+      <url>
+      <loc>https://podologjaworze.pl/blog</loc>
+      <lastmod>${localISOTime}</lastmod>
+      <changefreq>daily</changefreq>
+      <priority>0.8</priority>
+    </url>
     ${sitemapIndex}
   </urlset>
 `;
