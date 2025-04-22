6a2f9d60f7084b26106a670f7cbef7f6b3cca6b6
kvbaurb99 <kvbaurbanski2@gmail.com>
2025-04-22 22:09:25 +0200

    updates

--- src/components/header/styles/slide.ts
+++ src/components/header/styles/slide.ts
@@ -10,7 +10,7 @@ export const SliderHeader = styled.header`
 export const SlideWrapper = styled.div`
   position: relative;
   width: 100%;
-  height: 570px;
+  height: 580px;
 
   @media (min-width: 1280px) {
     height: 600px;
