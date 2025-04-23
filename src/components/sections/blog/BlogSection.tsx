acde5d61f88266df205829077423cb9637c93d3d
kvbaurb99 <kvbaurbanski2@gmail.com>
2025-04-23 19:51:34 +0200

    responsive blog section

--- src/components/sections/blog/BlogSection.tsx
+++ src/components/sections/blog/BlogSection.tsx
@@ -12,7 +12,7 @@ export default function BlogSection({ articles }: Props) {
   return (
     <SectionWrapper id="blog">
       <SectionTitle>Artykuły podologiczne</SectionTitle>
-      <div className="grid grid-cols-1 xl:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
         {articles.map((article) => (
           <ArticleCard
             key={article.id}
