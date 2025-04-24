fec6f95d47ffed6f8a65906bd1bc149c065acb74
kvbaurb99 <kvbaurbanski2@gmail.com>
2025-04-24 10:51:49 +0200

    fix build error

--- src/app/kategorie/[slug]/page.tsx
+++ src/app/kategorie/[slug]/page.tsx
@@ -6,43 +6,42 @@ import ArticleCard from "@/components/blog/card/ArticleCard";
 
 export const dynamic = "force-dynamic";
 
-type CategoryParams = {
-  params: {
-    slug: string;
-  };
-};
+type Props = Promise<{ slug: string }>;
 
-export default async function CategoryPageSingle({ params }: CategoryParams) {
-  const { slug } = params;
-
-  const [categories, articles] = await Promise.all([
-    getCategories(),
-    getArticles(),
-  ]);
-
+export default async function CategoryPageSingle({
+  params,
+}: {
+  params: Props;
+}) {
+  const { slug } = await params;
+  const categories = await getCategories();
+  const articles = await getArticles();
   const category = categories.find((item) => item.slug === slug);
   const filteredArticles = articles.filter(
     (item) => item.category === category?.name
   );
-
   return (
-    <SectionWrapper>
-      <SectionTitle>Kategoria {category?.name}</SectionTitle>
-      <div className="grid grid-cols-1 xl:grid-cols-3 gap-4 mt-4">
-        {filteredArticles.map((article) => (
-          <ArticleCard
-            key={article.slug}
-            title={article.title}
-            category={article.category}
-            category_slug={article.category_slug}
-            description={article.overview}
-            date="12.12.2023"
-            alt="test"
-            img={article.image}
-            slug={article.slug}
-          />
-        ))}
-      </div>
-    </SectionWrapper>
+    <>
+      <SectionWrapper>
+        <SectionTitle>Kategoria {category?.name}</SectionTitle>
+        <div className="grid grid-cols-1 xl:grid-cols-3 gap-4 mt-4">
+          {filteredArticles.map((article, i) => {
+            return (
+              <ArticleCard
+                key={i}
+                title={article.title}
+                category={article.category}
+                description={article.overview}
+                category_slug={article.category_slug}
+                date="12.12.2023"
+                alt="test"
+                img={article.image}
+                slug={article.slug}
+              />
+            );
+          })}
+        </div>
+      </SectionWrapper>
+    </>
   );
 }
