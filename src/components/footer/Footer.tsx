d928ff814086d0ccabea015c25236b0d32d13d86
kvbaurb99 <kvbaurbanski2@gmail.com>
2025-05-06 15:45:40 +0200

    updates

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -78,13 +78,6 @@ const Footer = () => {
             &copy; {new Date().getFullYear()} Zdrowe Stopy. Wszelkie prawa
             zastrzeżone.
           </p>
-          <a
-            rel="nofollow noopener noreferrer"
-            target="_blank"
-            href="https://urbanski-frontend.pl/"
-          >
-            <p className="mt-4 underline text-xs">urbanski-frontend.pl</p>
-          </a>
         </Copyright>
       </Container>
     </FooterWrapper>
